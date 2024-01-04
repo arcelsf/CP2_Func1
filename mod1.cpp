9cabd8cecc66b3a65ec3cf7b18f35790524dfa02
arcelsf <151742476+arcelsf@users.noreply.github.com>
2024-01-04 12:53:22 +0800

    Update mod1.cpp

--- mod1.cpp
+++ mod1.cpp
@@ -1,8 +1,7 @@
-// mod1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 using namespace std;
+
+int Addition(), Subtraction(), Multiplication(), Division();
 
 int main()
 {
@@ -11,19 +10,19 @@ int main()
     std::cout << "Welcome!\n\n";
     std::cout << "Enter The First Value\n\n";
     while (!(cin >> value1)) {
-        cin.clear();  // clear the error flag
+        cin.clear();  
         cin.ignore(numeric_limits<streamsize>::max(), '\n');  
         cout << "Invalid input. Please enter a valid number: ";
     }
     std::cout << "\nEnter The Second Value\n\n";
     while (!(cin >> value2)) {
-        cin.clear();  // clear the error flag
+        cin.clear(); 
         cin.ignore(numeric_limits<streamsize>::max(), '\n');  
         cout << "Invalid input. Please enter a valid number: ";
     }
     std::cout << "\n[1]Addition\n[2]Subtraction\n[3]Multiplication\n[4]Division\n\n";
     while (!(cin >> choice) || choice < 1 || choice > 4) {
-        cin.clear();  // clear the error flag
+        cin.clear();  
         cin.ignore(numeric_limits<streamsize>::max(), '\n');  
         cout << "Invalid input. Please enter a valid number: ";
     }
@@ -45,7 +44,7 @@ int main()
         std::cout << "The Product is " << value1 * value2 << ".\n\n";
         break;
     case 4:
-        std::cout << "The Product is " << value1 / value2 << ".\n\n";
+        std::cout << "The Qoutient is " << value1 / value2 << ".\n\n";
         break;
     default:
         std::cout << "Invalid Input\n\n";
@@ -70,14 +69,77 @@ int main()
         cout << "Thank you and have a nice day!";
         break;
     }
+    return 0;
+}
 
-      
-    
+int Addition() {
+    int value1, value2;
+    std::cout << "Enter The First Value\n\n";
+    while (!(cin >> value1)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid input. Please enter a valid number: ";
+    }
+    std::cout << "\nEnter The Second Value\n\n";
+    while (!(cin >> value2)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid input. Please enter a valid number: ";
+    }
+    std::cout << "The Sum of " << value1 << " and " << value2 << " is " << value1 + value2 << ". ";
+    return 0;
+}
 
-    
+int Subtraction() {
+    int value1, value2;
+    std::cout << "Enter The First Value\n\n";
+    while (!(cin >> value1)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid input. Please enter a valid number: ";
+    }
+    std::cout << "\nEnter The Second Value\n\n";
+    while (!(cin >> value2)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid input. Please enter a valid number: ";
+    }
+    std::cout << "The Difference of " << value1 << " and " << value2 << " is " << value1 - value2 << ". ";
+    return 0;
+}
 
-
-
+int Multiplication() {
+    int value1, value2;
+    std::cout << "Enter The First Value\n\n";
+    while (!(cin >> value1)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid input. Please enter a valid number: ";
+    }
+    std::cout << "\nEnter The Second Value\n\n";
+    while (!(cin >> value2)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid input. Please enter a valid number: ";
+    }
+    std::cout << "The Product of " << value1 << " and " << value2 << " is " << value1 * value2 << ". ";
+    return 0;
+}
+int Division() {
+    int value1, value2;
+    std::cout << "Enter The First Value\n\n";
+    while (!(cin >> value1)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid input. Please enter a valid number: ";
+    }
+    std::cout << "\nEnter The Second Value\n\n";
+    while (!(cin >> value2)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid input. Please enter a valid number: ";
+    }
+    std::cout << "The Qoutient of " << value1 << " and " << value2 << " is " << value1 / value2 << ". ";
     return 0;
 }
 
